87b43593e7743b0a21727f584a562990963bb9b7
DoctorAndy <1319337351@qq.com>
2025-02-23 22:12:48 +0800

    把数据库迁移到28

--- backend/conf/env.py
+++ backend/conf/env.py
@@ -15,13 +15,13 @@ DATABASE_ENGINE = "django.db.backends.mysql"
 DATABASE_NAME = 'django-vue3-admin' # mysql 时使用
 
 # 数据库地址 改为自己数据库地址
-DATABASE_HOST = '127.0.0.1'
+DATABASE_HOST = '192.168.9.28'
 # # 数据库端口
 DATABASE_PORT = 3306
 # # 数据库用户名
 DATABASE_USER = "root"
 # # 数据库密码
-DATABASE_PASSWORD = '87651361'
+DATABASE_PASSWORD = 'dongxu666'
 
 # 表前缀
 TABLE_PREFIX = "dvadmin_"
