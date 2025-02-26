1b8d924a3eb06d45fad5ed3dd937a39afdd94d1c
DoctorAndy <1319337351@qq.com>
2025-02-26 15:06:48 +0800

    feat:后端：后端更改数据库字段

--- backend/data_manager/models.py
+++ backend/data_manager/models.py
@@ -10,11 +10,7 @@ class DatasetManagerModel(CoreModel):
     """
     id = models.BigAutoField(primary_key=True,verbose_name="数据集Id")
     name = models.CharField(max_length=50, verbose_name="数据集名称", unique=True)
-    description = models.TextField(verbose_name="数据集描述", blank=True, null=True)
     data = models.CharField(max_length=255, verbose_name="数据集文件路径", blank=True, null=True)
-    create_time = models.DateTimeField(auto_now_add=True, verbose_name="数据集创建时间")
-    update_time = models.DateTimeField(auto_now=True, verbose_name="数据集更新时间")
-    owner_id = models.IntegerField(verbose_name="数据集拥有者id", blank=True, null=True)
     type = models.CharField(max_length=50, verbose_name="数据集类型", blank=True, null=True)
 
 
